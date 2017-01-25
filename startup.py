9c8b01fc56e0cf2ee3750f2318b2904ae48f8ac5
giuliojiang <giuliojiang@gmail.com>
2017-01-25 09:12:56 +0000

    add configuration mouse for Synaptics touchpad

--- startup.py
+++ startup.py
@@ -8,7 +8,7 @@ import subprocess
 import time
 import sys
 
-MOUSE_XINPUT_NAME = "Xornet"
+MOUSE_XINPUT_NAME = ["Xornet", "Synaptics"]
 
 def run_command_shell_proc(cmd):
     print('>>> {}'.format(cmd))
@@ -18,28 +18,37 @@ def run_command_shell(cmd):
     the_process = run_command_shell_proc(cmd)
     the_process.wait()
 
+def any_contains(matches, candidate):
+    for match in matches:
+        if match in candidate:
+            return True
+    return False
+
 # disable mouse acceleration
 run_command_shell('xset m 0/1 4')
 
 # retrieve list of devices
 xinput_process = run_command_shell_proc('xinput list')
-mouse_xinput_line = ''
+mouse_xinput_lines = []
 while True:
     line = xinput_process.stdout.readline()
     if line != '':
-        if MOUSE_XINPUT_NAME in line:
-            mouse_xinput_line = line.rstrip()
+        if any_contains(MOUSE_XINPUT_NAME, line):
+            mouse_xinput_lines.append(line.rstrip())
     else:
         break
 xinput_process.wait()
 
 # guess the number of the mouse device
-try:
-    mouse_xinput_id = int(mouse_xinput_line.split("id=")[1].split('\t')[0])
-except:
-    print('Could not guess id of mouse from line:')
-    print(mouse_xinput_line)
-    sys.exit(1)
+mouse_xinput_ids = []
+for mouse_line in mouse_xinput_lines:
+    try:
+        mouse_xinput_id = int(mouse_line.split("id=")[1].split('\t')[0])
+        mouse_xinput_ids.append(mouse_xinput_id)
+    except:
+        print('Could not guess id of mouse from line:')
+        print(mouse_line)
 
 # set mouse speed
-run_command_shell('xinput set-prop {} "Device Accel Constant Deceleration" 2'.format(mouse_xinput_id))
+for mouse_id in mouse_xinput_ids:
+    run_command_shell('xinput set-prop {} "Device Accel Constant Deceleration" 2'.format(mouse_id))
