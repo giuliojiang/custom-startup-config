e09c67af0d9a91e509844dd378251e093c822ea8
Giulio Jiang <giuliojiang@gmail.com>
2017-12-23 12:59:36 +0000

    Add okular to install list

--- autorestore/install.sh
+++ autorestore/install.sh
@@ -3,7 +3,7 @@
 # Install required packages ---------------------------------------------------
 apt update
 apt-get -y upgrade
-apt-get -y install kate git libinput10 xserver-xorg-input-libinput screen nano gedit python chromium-browser guake gitk build-essential vlc gimp make
+apt-get -y install kate git libinput10 xserver-xorg-input-libinput screen nano gedit python chromium-browser guake gitk build-essential vlc gimp make okular
 
 # Call the setup script -------------------------------------------------------
 ./setup.py
