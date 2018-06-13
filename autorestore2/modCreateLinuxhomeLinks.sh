52f21cf708bf9fe325278405b7c96a1c31aa6554
giuliojiang <giuliojiang@gmail.com>
2018-06-13 16:32:30 +0100

    Add create links

---
+++ autorestore2/modCreateLinuxhomeLinks.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+ln -s /run/media/gj/LINUXHOME/conda conda
+ln -s /run/media/gj/LINUXHOME/documents Documents
+ln -s /run/media/gj/LINUXHOME/git git
+ln -s /run/media/gj/LINUXHOME/pictures Pictures
