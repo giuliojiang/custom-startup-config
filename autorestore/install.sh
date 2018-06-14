f25bf7cd9cafea268b1ae898a8140554030f619f
giuliojiang <giuliojiang@gmail.com>
2018-06-14 11:33:31 +0100

    Update install list

--- autorestore/install.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-# Install required packages ---------------------------------------------------
-apt update
-apt-get -y upgrade
-apt-get -y install kate git libinput10 xserver-xorg-input-libinput screen nano gedit python chromium-browser guake gitk build-essential vlc gimp make okular
--- autorestore2/modOpensuseInstalls.sh
+++ autorestore2/modOpensuseInstalls.sh
@@ -1,2 +1,2 @@
 zypper refresh
-zypper install texlive-latex texlive-appendix texlive-wrapfig texlive-smartdiagram kate gcc-c++ cmake screen nano git python python3 shotwell nemo gimp gitg evince htop
+zypper install texlive-latex texlive-appendix texlive-wrapfig texlive-smartdiagram kate gcc-c++ cmake screen nano git python python3 shotwell nemo gimp gitg evince htop yakuake texlive-todonotes nodejs
