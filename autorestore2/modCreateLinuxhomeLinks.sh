85b9e912b9a5afe90743fff5853d1632c2e2968d
giuliojiang <giuliojiang@gmail.com>
2018-06-14 10:43:11 +0100

    UPdate linker

--- autorestore2/modCreateLinuxhomeLinks.sh
+++ autorestore2/modCreateLinuxhomeLinks.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
+rm -rf conda
+rm -rf Documents
+rm -rf git
+rm -rf Pictures
+rm -rf Videos
 ln -s /run/media/gj/LINUXHOME/conda conda
 ln -s /run/media/gj/LINUXHOME/documents Documents
 ln -s /run/media/gj/LINUXHOME/git git
 ln -s /run/media/gj/LINUXHOME/pictures Pictures
+ln -s /run/media/gj/LINUXHOME/videos Videos
